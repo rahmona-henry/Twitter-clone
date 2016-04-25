92d0ecbcabbb9a977cf932d08aead4f6f832a270
Jess Stuart & maxine-ellah <pair+jess-of-the-woods.maxine-ellah@devbootcamp.com>
2016-04-25 13:42:23 +1200

    refactor, move code to config.js, redirect to /secret on post sign-up route

---
+++ config.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var hbs = require('express-hbs')
+var session = require('express-session')
+
+var app = express();
+var path = require('path');
+var knex = require('knex')
+var bodyParser = require('body-parser')
+
+app.use(express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended:true }))
+
+
+
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: './dev.sqlite3'
+  },
+  useNullAsDefault: true
+})
+
+app.use(session({
+  secret: 'ssshhhhhh! Top secret!',
+  saveUninitialized: true,
+  resave: true,
+  db: knex
+}))
+
+module.exports = {
+  app: app,
+  knex: knex
+}
--- server.js
+++ server.js
@@ -1,34 +1,8 @@
-var express = require('express');
-// var hbs = require('hbs');
-var hbs = require('express-hbs')
-var session = require('express-session')
-
-var app = express();
-var path = require('path');
-var knex = require('knex')
-var bodyParser = require('body-parser')
 var bcrypt = require('bcrypt')
+var config = require('./config.js')
+var knex = config.knex
+var app = config.app
 
-app.use(express.static("public"));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({ extended:true }))
-
-
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: './dev.sqlite3'
-  },
-  useNullAsDefault: true
-})
-
-app.use(session({
-  secret: 'ssshhhhhh! Top secret!',
-  saveUninitialized: true,
-  resave: true,
-  db: knex
-}))
 
 //=========================================//
 //========== GET routes ===================//
@@ -39,7 +13,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/newTweet', function(req, res) {
-  res.render('tweetPost', {id: req.session.userId})
+  res.render('tweetPost', { id: req.session.userId })
 //  console.log('this is req.session: ', req.session)
 })
 
@@ -52,7 +26,7 @@ app.get('/signIn', function (req, res) {
 });
 
 app.get('/secret', function(req, res){
-    res.render('secret', {id: req.session.userId})
+    res.render('secret', { id: req.session.userId })
 })
 
 app.get('/allTweets', function (req, res) {
@@ -73,7 +47,7 @@ app.get('/signOut', function (req, res) {
 
 app.post('/newTweet', function (req, res) {
     // console.log('this is req.body:', req.body)
-  knex('tweets').insert({tweeted: req.body.tweeted})
+  knex('tweets').insert({ tweeted: req.body.tweeted })
   .then(function(data){
     res.redirect('allTweets')
     console.log('success')
@@ -82,15 +56,13 @@ app.post('/newTweet', function (req, res) {
 
 app.post('/signIn', function(req, res){
   knex.select().table('users')
-//  knex('users').where({ email: req.body.email })
     .then(function(data) {
-      // console.log('this is req.body: ', req.body)
       console.log('data', data[0].id)
       // console.log(data)
       // for (i = 0; i < data.length; i++){
       //   console.log(data[i].hashed_password)
       // }
-      if (bcrypt.compareSync(req.body.password, data[0].hashed_password)) {
+      if (bcrypt.compareSync( req.body.password, data[0].hashed_password )) {
         req.session.userId = data[0].id
         res.redirect('/secret')
         console.log('success')
@@ -112,7 +84,7 @@ app.post('/signUp', function (req, res) {
   .then(function(data){
     console.log('this is data from sign-up', data)
     req.session.userId = data
-    res.redirect('/')
+    res.redirect('/secret')
   })
   .catch(function(error){
     console.log(error, 'problem')
