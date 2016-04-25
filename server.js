3df5ca946ac5a25540d4b6f44891b9d8c9e66ce8
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-04-25 20:38:23 +1200

    clearer console.logs, tidying up layout, spacing, changed id to userId in server & some views

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/signIn', function (req, res) {
 });
 
 app.get('/secret', function(req, res){
-    res.render('secret', { id: req.session.userId })
+    res.render('secret', { userId: req.session.userId })
 })
 
 app.get('/allTweets', function (req, res) {
@@ -58,7 +58,7 @@ app.get('/user/:id', function (req, res) {
 app.get('/user/:id/follow', function(req, res){
   knex('follows').insert({followerId: req.session.userId, followingId: req.params.id})
   .then(function(data){
-    console.log('success (bam!)')
+    console.log('success. (bam!)')
   })
 })
 
@@ -74,24 +74,27 @@ app.get('/signOut', function (req, res) {
 
 app.post('/newTweet', function (req, res) {
     // console.log('this is req.body:', req.body)
-    console.log('req.session: ', req.session)
+    // console.log('req.session: ', req.session)
   knex('tweets').insert({ tweeted: req.body.tweeted, userId: req.session.userId })
   .then(function(data){
     res.redirect('allTweets')
-    console.log('success')
+    console.log('Success! Creet posted by cretin #' + req.session.userId + '!')
   })
 })
 
 app.post('/signIn', function(req, res){
   knex('users').where('email', req.body.email)
     .then(function(data) {
-      console.log('data[0].id', data[0].id)
+      console.log('this is data: ', data)
+  //    console.log('data[0].id:', data[0].id)
+  //    console.log('req.body.password: ', req.body.password)
       if (bcrypt.compareSync( req.body.password, data[0].hashed_password )) {
         req.session.userId = data[0].id
         res.redirect('/secret')
-        console.log('success')
+        console.log('success! sign in happened by cretin #' + req.session.userId +'!')
       }
       else {
+        console.log('incorrect password')
         res.redirect('/signIn')
       }
     })
@@ -106,7 +109,7 @@ app.post('/signUp', function (req, res) {
   var hash = bcrypt.hashSync( req.body.password, 10 )
   knex('users').insert({ email: req.body.email, hashed_password: hash })
   .then(function(data){
-    console.log('this is data from sign-up', data)
+    console.log('this is "data" from sign-up', data)
     req.session.userId = data
     res.redirect('/secret')
   })
